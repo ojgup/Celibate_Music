1d9f242f2683d3881ff0fe87d8944f036ce523c2
ojgup <48079091+ojgup@users.noreply.github.com>
2019-11-22 09:02:49 +1100

    Added starter files.

---
+++ Celibate_Music_API/Celibate_Music_API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Celibate_Music_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Celibate_Music_API/Celibate_Music_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Celibate_Music_API.WebApiApplication" Language="C#" %>
